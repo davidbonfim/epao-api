ec74179d9351d0e6dc1b2d3790832ca3baed276e
davidbonfim <david@historymakers.com.br>
2024-07-30 12:51:01 -0300

    Criando Controller do crud de EPAO

---
+++ app/Http/Controllers/EpaoController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EpaoController extends Controller
+{
+    public function create(){
+        dd('teste');
+    }
+}
