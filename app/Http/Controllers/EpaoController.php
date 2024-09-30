fcb9dc78cc1a210a282836caef298d8fcf4ebccf
David Bonfim <david@historymakers.com.br>
2024-09-30 10:28:11 -0300

    WIP: Create Epao

--- app/Http/Controllers/EpaoController.php
+++ app/Http/Controllers/EpaoController.php
@@ -2,11 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Epao;
 use Illuminate\Http\Request;
 
 class EpaoController extends Controller
 {
     public function create(){
-        dd('teste');
+        
+        $epao = Epao::create([    
+        'title' => 'Primeiro Epao',
+        'verse' => 'Jo 15:12',
+        'observation' => 'Love',
+        'application' => 'Love your brothers',
+        'pray' => 'Lord, help me',    
+    ]);
+    dd($epao);
     }
 }
